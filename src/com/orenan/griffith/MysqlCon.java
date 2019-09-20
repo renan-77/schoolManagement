78375a60887ef43d6de15457861c9eda7c2fa407
renan-77 <renan.deoliveiramonteiro@student.griffith.ie>
2019-09-20 10:31:37 +0100

    Comments added to mysql class

--- src/com/orenan/griffith/MysqlCon.java
+++ src/com/orenan/griffith/MysqlCon.java
@@ -7,7 +7,7 @@ import java.awt.EventQueue;
 
 public class MysqlCon {
 	
-	static String textOutput = "bla";
+	static String textOutput = "Blank";
 
 	public static void main(String[] args) {
 		
@@ -16,15 +16,19 @@ public class MysqlCon {
 		  
 	}
 	
+	//Function that connects to the db and looks for students based on their id.
 	public static String giveData(String id) {
 		try{  
+			//Driver connecting to the db using usr and pass.
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection con=DriverManager.getConnection(  
 			"jdbc:mysql://51.75.248.73:3306/storm","rmt_admin","V!HpE9zK");  
 
+			//Creating statement and a result set for it.
 			Statement stmt=con.createStatement();  
 			ResultSet rs=stmt.executeQuery("select * from student WHERE student.id = " + id);  
 			
+			//keep returning the result for the user until there's no next().
 			while(rs.next())
 				textOutput = "  " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5);  
 				System.out.println(textOutput);
@@ -36,18 +40,23 @@ public class MysqlCon {
 			return textOutput;
 	}
 	
+	//Function to register a new student.
 	public static void Register(String name, String address, int mobileNumber, String nationality) {
 		try {
+			//Calling the driver, Connecting to the database and creating the statement.
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://51.75.248.73:3306/storm","rmt_admin","V!HpE9zK");
 			Statement myStmt = con.createStatement();					
 			
+			//Passing the command to the database to add the student based on the input.
 			String sql = String.format("insert into student" 
 					+" (name, address, mobile_number, nationality)"
 					+" values ('%s','%s' ,'%d', '%s')", name, address, mobileNumber, nationality);
 			
+			//Executes the query.
 			myStmt.executeUpdate(sql);
 			
+			//Confirms that the user was added as a pop-up.
 			JOptionPane.showMessageDialog(null, "New user registred!");
 			
 		}catch (Exception e) {
